7c930ab979ca274cb30ebf581ba07ebda73a08e7
terminaljs <noahbroache@gmail.com>
2026-05-21 17:24:21 -0400

    Replace Pong game with Crazy Cattle 3D

--- app.js
+++ app.js
@@ -2,11 +2,265 @@
 // For the downloader to work perfectly, you can store the game's raw HTML/JS code inside the 'code' property.
 const games = [
     {
-        id: "pong",
-        title: "Classic Pong",
-        description: "A simple recreation of the classic arcade game.",
+        id: "crazycattle",
+        title: "Crazy Cattle 3D",
+        description: "A weird ass game about being a sheep and ramming into other sheep. Be the only sheep left standing.",
         // Example of a self-contained HTML game
-        code: `<!DOCTYPE html><html><head><title>Pong</title></head><body style="background:#000;color:#fff;text-align:center;"><h1>Pong Game</h1><p>Imagine a fun game of pong here!</p><script>console.log('Pong loaded');</script></body></html>`
+        code: `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script>!function(){const e="https://yoplay.io",n=window.location.href.includes(".embed");if(function isCrossOriginIframe3(){try{return window!==window.top&&window.top.location.hostname!==window.location.hostname}catch(e){return!0}}()){if(n||document.referrer.startsWith(e))return;{const e=`<style>html body * {display: none !important;opacity: 0 !important;}html body #home_box * {display: block !important;opacity: 1 !important;font-family: Helvetica, Arial, Roboto, sans-serif;}</style><div id="home_box" style="opacity: 1 !important;position: fixed;inset: 0px;display: flex !important;flex-direction: column;-webkit-box-align: center;align-items: center;-webkit-box-pack: center;justify-content: center;padding: 24px;text-align: center;">
+            <img src="https://yoplay.io/data/image/options/home-bg.jpg" alt="Background Image" title="Yoplay.io Background" style="position: absolute;inset: 0;width: 100%;height: 100%;object-fit: cover;z-index: -1;">
+            <h1 style="font-size: 27px;font-weight: 600;margin: 9px 0;color: #fff;text-transform: uppercase;text-shadow: 2px 2px 4px rgba(0,0,0,0.5);">Play Crazy Cattle 3d on</h1>
+            <a target="_blank" title="Home" href="https://yoplay.io"><img style="display: block;width: 200px;height: 100%;object-fit: cover;" src="https://yoplay.io/data/image/options/home-logo-web.png" alt="Logo Image" title="Yoplay.io Logo"></a>
+            <p style="color: #fff;font-size: 24px;font-weight: 500;margin-top: 9px;margin-bottom: 21px;font-style: italic;">for best experiment!</p>
+            <a target="_blank" title="Play Crazy Cattle 3d" href="https://yoplay.io/crazy-cattle-3d/" style="text-shadow: 2px 2px 4px rgba(0,0,0,0.5);text-decoration: none;background-color: rgba(255, 255, 255, 0.3);color: #fff;font-weight: 700;padding: 12px 32px;border: 1px solid #fff;border-radius: 3px;box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);transition: all 0.3s ease-in-out;transform: scale(1);cursor: pointer;">Play Now!</a>
+        </div>`;return void(document.body?(document.body.innerHTML=e,window.addEventListener=function(){},document.write=function(){},window.stop(),document.querySelectorAll("script").forEach((e=>e.remove()))):document.addEventListener("DOMContentLoaded",(()=>{document.body||document.querySelector("body")||document.documentElement.appendChild(document.createElement("body")),document.body.innerHTML=e,window.addEventListener=function(){},document.write=function(){},window.stop(),document.querySelectorAll("script").forEach((e=>e.remove()))})))}}n||document.referrer.startsWith(e)||(window.location.href="https://yoplay.io/crazy-cattle-3d/")}();</script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+	<title>CrazyCattle3D</title>
+	<style>
+		html,
+		body,
+		#canvas {
+			margin: 0;
+			padding: 0;
+			border: 0;
+		}
+
+		body {
+			color: white;
+			background-color: black;
+			overflow: hidden;
+			touch-action: none;
+		}
+
+		#canvas {
+			display: block;
+		}
+
+		#canvas:focus {
+			outline: none;
+		}
+
+		#status,
+		#status-splash,
+		#status-progress {
+			position: absolute;
+			left: 0;
+			right: 0;
+		}
+
+		#status,
+		#status-splash {
+			top: 0;
+			bottom: 0;
+		}
+
+		#status {
+			background-color: #000000;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			visibility: hidden;
+		}
+
+		#status-splash {
+			max-height: 100%;
+			max-width: 100%;
+			margin: auto;
+		}
+
+		#status-splash.show-image--false {
+			display: none;
+		}
+
+		#status-splash.fullsize--true {
+			height: 100%;
+			width: 100%;
+			object-fit: contain;
+		}
+
+		#status-splash.use-filter--false {
+			image-rendering: pixelated;
+		}
+
+		#status-progress,
+		#status-notice {
+			display: none;
+		}
+
+		#status-progress {
+			bottom: 10%;
+			width: 50%;
+			margin: 0 auto;
+		}
+
+		#status-notice {
+			background-color: #5b3943;
+			border-radius: 0.5rem;
+			border: 1px solid #9b3943;
+			color: #e0e0e0;
+			font-family: 'Noto Sans', 'Droid Sans', Arial, sans-serif;
+			line-height: 1.3;
+			margin: 0 2rem;
+			overflow: hidden;
+			padding: 1rem;
+			text-align: center;
+			z-index: 1;
+		}
+	</style>
+	<link id="-gd-engine-icon" rel="icon" type="image/png" href="index.icon.png" />
+	<link rel="apple-touch-icon" href="index.apple-touch-icon.png" />
+
+</head>
+
+<body>
+	<canvas id="canvas">
+		Your browser does not support the canvas tag.
+	</canvas>
+
+	<noscript>
+		Your browser does not support JavaScript.
+	</noscript>
+
+	<div id="status">
+		<img id="status-splash" class="show-image--true fullsize--true use-filter--true" src="index.png" alt="">
+		<progress id="status-progress"></progress>
+		<div id="status-notice"></div>
+	</div>
+
+	<script src="index.js"></script>
+	<script>
+		const GODOT_CONFIG = {
+			"args": [],
+			"canvasResizePolicy": 2,
+			"ensureCrossOriginIsolationHeaders": true,
+			"executable": "index",
+			"experimentalVK": false,
+			"fileSizes": {
+				"index.pck": 42336176,
+				"index.wasm": 43699190
+			},
+			"focusCanvas": true,
+			"gdextensionLibs": []
+		};
+		const GODOT_THREADS_ENABLED = false;
+		const engine = new Engine(GODOT_CONFIG);
+
+		(function() {
+			const statusOverlay = document.getElementById('status');
+			const statusProgress = document.getElementById('status-progress');
+			const statusNotice = document.getElementById('status-notice');
+
+			let initializing = true;
+			let statusMode = '';
+
+			function setStatusMode(mode) {
+				if (statusMode === mode || !initializing) {
+					return;
+				}
+				if (mode === 'hidden') {
+					statusOverlay.remove();
+					initializing = false;
+					return;
+				}
+				statusOverlay.style.visibility = 'visible';
+				statusProgress.style.display = mode === 'progress' ? 'block' : 'none';
+				statusNotice.style.display = mode === 'notice' ? 'block' : 'none';
+				statusMode = mode;
+			}
+
+			function setStatusNotice(text) {
+				while (statusNotice.lastChild) {
+					statusNotice.removeChild(statusNotice.lastChild);
+				}
+				const lines = text.split('\n');
+				lines.forEach((line) => {
+					statusNotice.appendChild(document.createTextNode(line));
+					statusNotice.appendChild(document.createElement('br'));
+				});
+			}
+
+			function displayFailureNotice(err) {
+				console.error(err);
+				if (err instanceof Error) {
+					setStatusNotice(err.message);
+				} else if (typeof err === 'string') {
+					setStatusNotice(err);
+				} else {
+					setStatusNotice('An unknown error occurred.');
+				}
+				setStatusMode('notice');
+				initializing = false;
+			}
+
+			const missing = Engine.getMissingFeatures({
+				threads: GODOT_THREADS_ENABLED,
+			});
+
+			if (missing.length !== 0) {
+				if (GODOT_CONFIG['serviceWorker'] && GODOT_CONFIG['ensureCrossOriginIsolationHeaders'] && 'serviceWorker' in navigator) {
+					let serviceWorkerRegistrationPromise;
+					try {
+						serviceWorkerRegistrationPromise = navigator.serviceWorker.getRegistration();
+					} catch (err) {
+						serviceWorkerRegistrationPromise = Promise.reject(new Error('Service worker registration failed.'));
+					}
+					// There's a chance that installing the service worker would fix the issue
+					Promise.race([
+						serviceWorkerRegistrationPromise.then((registration) => {
+							if (registration != null) {
+								return Promise.reject(new Error('Service worker already exists.'));
+							}
+							return registration;
+						}).then(() => engine.installServiceWorker()),
+						// For some reason, `getRegistration()` can stall
+						new Promise((resolve) => {
+							setTimeout(() => resolve(), 2000);
+						}),
+					]).then(() => {
+						// Reload if there was no error.
+						window.location.reload();
+					}).catch((err) => {
+						console.error('Error while registering service worker:', err);
+					});
+				} else {
+					// Display the message as usual
+					const missingMsg = 'Error\nThe following features required to run Godot projects on the Web are missing:\n';
+					displayFailureNotice(missingMsg + missing.join('\n'));
+				}
+			} else {
+				setStatusMode('progress');
+				engine.startGame({
+					'onProgress': function(current, total) {
+						if (current > 0 && total > 0) {
+							statusProgress.value = current;
+							statusProgress.max = total;
+						} else {
+							statusProgress.removeAttribute('value');
+							statusProgress.removeAttribute('max');
+						}
+					},
+				}).then(() => {
+					setStatusMode('hidden');
+				}, displayFailureNotice);
+			}
+		}());
+	</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v833ccba57c9e4d2798f2e76cebdd09a11778172276447" integrity="sha512-57MDmcccJXYtNnH+ZiBwzC4jb2rvgVCEokYN+L/nLlmO8rfYT/gIpW2A569iJ/3b+0UEasghjuZH/ma3wIs/EQ==" data-cf-beacon='{"version":"2024.11.0","token":"a6eeeef71805413e99ae7ee5fad90bc4","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
+</body>
+
+</html>`
+/* ---END OF CODE--- */
+
+
+
+
+
     },
     {
         id: "snake",
